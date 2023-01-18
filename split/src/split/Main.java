a3d3842b952cbd535757bbf554b1555725c7b790
Kain Samuel <kaiansamuelster@gmail.com>
2023-01-17 21:35:14 -0300

    Primeiro Commit

---
+++ split/src/split/Main.java
@@ -0,0 +1,15 @@
+package split;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		String s = "potato orange apple lemon";
+		
+		String[] vect = s.split(" ");
+		
+		System.out.println(vect[0]);
+
+	}
+
+}
